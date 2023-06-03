9796621f346d28f85d787ff44c79d4c09011b231
Carlos Montalvo <cmontalvo@southalabama.edu>
2023-06-03 17:06:05 -0500

    edited some classroom codes for ladiga

---
+++ classroom/get_time.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from datetime import datetime
+go = True
+while go:
+    val = raw_input('Bib Number? ')
+    if val == 'q':
+        go = False
+    now = datetime.now()
+    dt_string = now.strftime('%H:%M:%S')
+    print(val,dt_string)
